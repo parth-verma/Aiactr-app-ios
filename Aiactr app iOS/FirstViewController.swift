39a8e32c919fc7cf30131be5622161c3e4b6db73
Parth Verma <v.parth98@gmail.com>
2017-04-28 17:03:21 +0530

    Initial Commit

---
+++ Aiactr app iOS/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Aiactr app iOS
+//
+//  Created by Parth Verma on 28/04/17.
+//  Copyright © 2017 Parth Verma. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Aiactr app iOS/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Aiactr app iOS
+//
+//  Created by Parth Verma on 28/04/17.
+//  Copyright © 2017 Parth Verma. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
